03d3f5dca6e5473891f64e31935573960e96bc76
luigi iadicola <luigiadicola@gmail.com>
2023-10-22 16:37:41 +0200

    install react (node)

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model {
 
     use HasFactory;
+    protected $table = 'products';
 
     protected $fillable = ['title', 'description', 'created_at'];
+
 }
 
 ?>
